d4c496d60d625b1ba6886cd8fd312af79bd2afc7
Sander Declerck <sander@sdsoftware.be>
2026-01-21 09:14:44 +0100

    Add mac os installation

--- packages/safe-chain/src/installation/installUltimate.js
+++ packages/safe-chain/src/installation/installUltimate.js
@@ -2,6 +2,7 @@ import { platform } from "os";
 import { ui } from "../environment/userInteraction.js";
 import { initializeCliArguments } from "../config/cliArguments.js";
 import { installOnWindows } from "./installOnWindows.js";
+import { installOnMacOS } from "./installOnMacOS.js";
 
 export async function installUltimate() {
   initializeCliArguments(process.argv);
@@ -10,6 +11,8 @@ export async function installUltimate() {
 
   if (operatingSystem === "win32") {
     await installOnWindows();
+  } else if (operatingSystem === "darwin") {
+    await installOnMacOS();
   } else {
     ui.writeInformation(
       `${operatingSystem} is not supported yet by SafeChain's ultimate version.`,
