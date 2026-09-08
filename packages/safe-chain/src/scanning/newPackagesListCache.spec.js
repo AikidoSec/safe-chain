6324f1414d4e4032d8d73e81fc8d9f189750f5d1
Sander Declerck <sander.declerck@aikido.dev>
2026-09-08 18:59:58 +0200

    Add mock for defaultMalwareListBaseUrl

--- packages/safe-chain/src/scanning/newPackagesListCache.spec.js
+++ packages/safe-chain/src/scanning/newPackagesListCache.spec.js
@@ -23,6 +23,7 @@ mock.module("../config/settings.js", {
     getMalwareListBaseUrl: () => "https://malware-list.aikido.dev",
     ECOSYSTEM_JS: "js",
     ECOSYSTEM_PY: "py",
+    defaultMalwareListBaseUrl: "https://malware-list.aikido.dev",
   },
 });
 
